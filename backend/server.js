69e4f835d8312bd26077d3f6eb401876c5d6bd96
db-malik <dbmalek92@egmail.com>
2022-10-05 23:50:33 +0100

    add color to console message

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} on port${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port${PORT}`.cyan.underline
+  )
 )
