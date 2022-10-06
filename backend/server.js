a980e244dfbe062f159f07b4a4c1f6e09d609f0f
db-malik <dbmalek92@egmail.com>
2022-10-06 01:53:23 +0100

    add productRoutes to server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 const app = express()
@@ -12,16 +12,8 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-// Get all products
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-// get product by id
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
 const PORT = process.env.PORT || 5000
 
 app.listen(
