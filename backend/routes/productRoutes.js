d36c30da42193faef0d046e248f16d40ddcc78ef
db-malik <dbmalek92@egmail.com>
2022-10-06 01:57:11 +0100

    get all products router

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,19 +1,26 @@
 import express from 'express'
+import asyncHandler from 'express-async-handler'
 const router = express.Router()
 
 import Product from '../models/productModel.js'
 
 // Get all products
-router.get('/', async (req, res) => {
-  const products = await Product.find({})
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
 
-  res.json(products)
-})
+    res.json(products)
+  })
+)
 
 // get product by id
-router.get('/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+  })
+)
 
 export default router
