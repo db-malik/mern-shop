3a48daf9b5f8f0d6b4ba6b911f7c4f117044a242
db-malik <dbmalek92@egmail.com>
2022-10-07 23:43:59 +0100

    cart route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,10 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="card" element={<CartPage />} />
+            <Route path="cart" element={<CartPage />}>
+              <Route path=":id" element={<CartPage />} />
+            </Route>
+
             <Route path="signIn" element={<SignInPage />} />
 
             <Route path="products">
