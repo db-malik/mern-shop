b186c2c5c43715e2838778f8e1e564a768b4558f
db-malik <dbmalek92@egmail.com>
2022-10-07 01:01:40 +0100

    import constanst productDetails

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,9 +3,12 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
-export const listProductsAction = () => async (dispatch) => {
+export const productsListAction = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
     const { data } = await axios.get('/api/products')
@@ -13,6 +16,24 @@ export const listProductsAction = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.masage,
+    })
+  }
+}
+
+export const productDetailsAction = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
