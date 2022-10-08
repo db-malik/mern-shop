b11407eaf77777e653a36e3afe710b500ca5774b
db-malik <dbmalek92@egmail.com>
2022-10-08 12:57:29 +0100

    add cartReducer to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,10 +5,11 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers'
-
+import { cartReducer } from './reducers/cartReducers'
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
 const initialState = {}
 
