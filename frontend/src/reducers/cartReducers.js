d60dac9023866900f58b387329bf7c3c41169af7
db-malik <dbmalek92@egmail.com>
2022-10-09 22:52:39 +0100

    remove item from cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,16 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  LOAD_CART_ITEMS,
+} from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
+    case LOAD_CART_ITEMS:
+      return {
+        ...state,
+        cartItems: action.payload,
+      }
     case CART_ADD_ITEM:
       const item = action.payload
       const existItem = state.cartItems.find((x) => x.id === item.id)
@@ -18,6 +27,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           ...state,
           cartItems: [...state.cartItems, item],
         }
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.id !== action.payload),
       }
 
     default:
