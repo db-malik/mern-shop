547813b131c78ac09289612c13e7c5c3d79892a7
db-malik <dbmalek92@gmail.com>
2022-10-13 00:55:01 +0100

    users/login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,6 @@ import express from 'express'
 import { authUser } from '../controllers/userController.js'
 const router = express.Router()
 
-router.get('/', authUser)
+router.post('/login', authUser)
 
 export default router
