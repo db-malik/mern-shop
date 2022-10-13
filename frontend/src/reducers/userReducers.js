61c503f961557c658d76ff9865b8271b992436c8
db-malik <dbmalek92@gmail.com>
2022-10-13 21:44:32 +0100

    add userRegisterReducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = { userInfo: {} }, action) => {
@@ -18,6 +21,23 @@ export const userLoginReducer = (state = { userInfo: {} }, action) => {
       return { loading: false, error: action.payload }
     case USER_LOGIN_LOGOUT:
       return { loading: false, userInfo: {} }
+    default:
+      return state
+  }
+}
+
+export const userRegisterReducer = (state = { userInfo: {} }, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true, userInfo: {} }
+    case USER_REGISTER_SUCCESS:
+      return {
+        loading: false,
+        userInfo: action.payload,
+      }
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload }
+
     default:
       return state
   }
