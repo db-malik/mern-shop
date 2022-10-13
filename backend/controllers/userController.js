91d4f63e64ed03d411633279b01f9efcd56e08d5
db-malik <dbmalek92@gmail.com>
2022-10-13 01:26:55 +0100

    user controller authUsr & getProfile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,44 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../routes/utils/generateToken.js'
 
-// @desc Fetch all products
-// @route Get  /api/products
+// @desc Auth user & get token
+// @route Post  /api/users
 // @access public
-const authUser = asyncHandler(async (req, res) => {})
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
 
-export { authUser }
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+})
+
+// @desc Get user profile
+// @route GET api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
