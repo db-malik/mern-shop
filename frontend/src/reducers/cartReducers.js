d262254ea604edf75961bb9767c52134adbd6b93
db-malik <dbmalek92@gmail.com>
2022-10-13 11:30:12 +0100

    use of initial state

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,16 +1,7 @@
-import {
-  CART_ADD_ITEM,
-  CART_REMOVE_ITEM,
-  LOAD_CART_ITEMS,
-} from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
-    case LOAD_CART_ITEMS:
-      return {
-        ...state,
-        cartItems: action.payload,
-      }
     case CART_ADD_ITEM:
       const item = action.payload
       const existItem = state.cartItems.find((x) => x.id === item.id)
