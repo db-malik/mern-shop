765f045f9a53e732d9290f6b9f278a51a919130c
db-malik <dbmalek92@gmail.com>
2022-10-13 17:04:15 +0100

    dropdown menu for user

--- frontend/src/containers/header/Header.js
+++ frontend/src/containers/header/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+  const logoutHandler = () => {}
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -19,11 +23,23 @@ const Header = () => {
                   <i className="fa-solid fa-cart-shopping"></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/signIn">
-                <Nav.Link>
-                  <i className="fa-solid fa-user"></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {Object.keys(userInfo).length !== 0 ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to={'/profile'}>
+                    <NavDropdown.Item> Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    {' '}
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <i className="fa-solid fa-user"></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
