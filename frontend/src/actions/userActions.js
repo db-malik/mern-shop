8de0f25c8d3cf6f7b8a11025b9a0925c599fab00
db-malik <dbmalek92@gmail.com>
2022-10-13 21:42:46 +0100

    create register constants

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from '../constants/userConstants'
 
 export const userLoginAction = (email, password) => async (dispatch) => {
@@ -37,3 +40,32 @@ export const logoutUserAction = () => (dispatch) => {
   dispatch({ type: USER_LOGIN_LOGOUT })
   localStorage.removeItem('userInfo')
 }
+
+export const userRegisterAction =
+  (name, email, password) => async (dispatch) => {
+    try {
+      dispatch({ type: USER_REGISTER_REQUEST })
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      const { data } = await axios.post(
+        '/api/users/register',
+        { name, email, password },
+        config
+      )
+
+      dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
+      localStorage.setItem('userInfo', JSON.stringify(data))
+      dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
+    } catch (error) {
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.masage,
+      })
+    }
+  }
