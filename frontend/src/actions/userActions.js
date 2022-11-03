7021a56c30f6bc3d91101ef6fb3dec3063370b24
Malik <dbmalek92@gmail.com>
2022-11-03 16:26:36 +0100

    getUserDetail action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,10 +3,15 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
-  USER_LOGIN_LOGOUT,
+  USER_LOGOUT,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
 } from '../constants/userConstants'
 
 export const userLoginAction = (email, password) => async (dispatch) => {
@@ -31,14 +36,46 @@ export const userLoginAction = (email, password) => async (dispatch) => {
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
-          : error.masage,
+          : error.message,
     })
   }
 }
 
-export const logoutUserAction = () => (dispatch) => {
-  dispatch({ type: USER_LOGIN_LOGOUT })
-  localStorage.removeItem('userInfo')
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/users/${id}`, config)
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logoutUserAction())
+    }
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload: message,
+    })
+  }
 }
 
 export const userRegisterAction =
@@ -57,15 +94,65 @@ export const userRegisterAction =
       )
 
       dispatch({ type: USER_REGISTER_SUCCESS, payload: data })
-      localStorage.setItem('userInfo', JSON.stringify(data))
+
       dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+
+      localStorage.setItem('userInfo', JSON.stringify(data))
     } catch (error) {
       dispatch({
         type: USER_REGISTER_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
-            : error.masage,
+            : error.message,
       })
     }
   }
+
+export const userUpdateProfileAction = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_PROFILE_REQUEST })
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/users/profile`, user, config)
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+      payload: data,
+    })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logoutUserAction())
+    }
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const logoutUserAction = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  localStorage.removeItem('cartItems')
+  // localStorage.removeItem('shippingAddress')
+  // localStorage.removeItem('paymentMethod')
+  dispatch({ type: USER_LOGOUT })
+  document.location.href = '/login'
+}
