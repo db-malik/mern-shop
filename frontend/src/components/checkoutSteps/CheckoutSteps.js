df1b7d5386cebd8448967bbda407ecf3685c3641
Malik <dbmalek92@gmail.com>
2022-11-04 22:15:24 +0100

    add checkOut

--- frontend/src/components/checkoutSteps/CheckoutSteps.js
+++ frontend/src/components/checkoutSteps/CheckoutSteps.js
@@ -3,7 +3,7 @@ import { Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
-    <Nav style={{ margin: '20px', display: 'flex', justifyContent: 'center' }}>
+    <Nav style={{ margin: '10px', display: 'flex', justifyContent: 'center' }}>
       <Nav.Item>
         {step1 ? (
           <LinkContainer to="/login">
